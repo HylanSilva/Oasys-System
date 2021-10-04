7e3136025f5d1842139902293af201fb7ca88fbd
Hylan Silva <hylansilva27@gmail.com>
2021-10-04 20:36:10 -0300

    vercao pre-alpha 0.0.3

--- js/script.js
+++ js/script.js
@@ -9,3 +9,15 @@ function apagar() {
     var barcode = document.getElementById('bar-code-view').innerHTML;
     document.getElementById('bar-code-view').innerHTML = barcode.substring(0, barcode.length - 1)
 }
+
+let input = document.querySelector("#bar-code-view");
+let button = document.querySelector("#btn-devolver");
+button.disabled = true;
+input.addEventListener("change", stateHandle);
+function stateHandle() {
+  if (document.querySelector("#bar-code-view").value === "") {
+    button.disabled = true; 
+  } else {
+    button.disabled = false;
+  }
+}

8f65d8795ee086a663e9179224eeeb227c77d276
Hylan Silva <hylansilva27@gmail.com>
2022-03-23 16:20:58 -0300

    adding new lybraries

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+## In English
+
 # Hello It's a Web View of Oasys, a system of libraryes
+
+---
+
+## Em Portugês
+
+# Olá! Está é a página web do Oasys, um sistema de genrenciamento de bibliotecas
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components"
+import ReactTypingEffect from 'react-typing-effect';
 
-const Div = styled.div`
-      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;      padding: 0;
+const Corpo = styled.div`
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
       margin: 0;
 `;
 
@@ -21,14 +22,11 @@ const Login = styled.button`
       margin-left: 65%;
       margin-top: 1.5%;
       margin-right: 20px;
-      :hover{
-        border: 3px solid black;
-        background-color: white;
-        color: black;
-      }
+      cursor: pointer;
 `;
 
 const Buscar = styled.button`
+      text-decoration: none;
       border: 3px solid black;
       background-color: white;
       color: black;
@@ -36,8 +34,8 @@ const Buscar = styled.button`
       height: 60px;
       border-radius: 25px;
       font-size: 24px;
-      transition: ease-in all;
-      animation-duration: 3.ms;
+      cursor: pointer;
+      transition: .3s ease all;
       :hover{
         border: 3px solid white;
         background-color: black;
@@ -58,35 +56,39 @@ const Parameter = styled.p`
       margin-left: 2%;
       font-weight: lighter;
       font-size: 38px;
+      overflow: hidden
 `;
 
-
 const Botton = styled.footer`
+      position: fixed;
+      bottom: 0;
       background-color: black;
       width: 100%;
       height: 25px;
       align-items: center;
       display: flex;
       flex-direction: column;
-      margin-top:8.3%;
-      padding-bottom: 0.5%;
 `;
 
 const TextBotton = styled.p`
+      margin-top: -1px;
       font-size: 18px;
-      margin-top: 0.25%;
       color: white;
 `;
 function App() {
 
 
   return (
-    <Div>
+    <Corpo>
       <nav>
         <Letreiro>Oasys</Letreiro>
       </nav>
       <main>
-        <Parameter>O maior acervo de livros<br/>se encontra aqui! </Parameter>
+        <Parameter>
+        <ReactTypingEffect
+        text={["O maior acervo de livros","você encontra aqui!"]} cursor={["_"]}/>
+        <br/>
+        </Parameter>
         <Carousel>
               <img style={{"width":"100%"}} src="./src/components/img/imagen.jpg"></img>
         </Carousel>
@@ -96,9 +98,11 @@ function App() {
         </div>
       </main>
       <Botton>
-        <TextBotton> Desenvolvido por: HylanSilva (DSLabz) </TextBotton>
+        <TextBotton> Desenvolvido por: <a
+        style={{"textDecoration":"none","color":"white"}} href="https://github.com/HylanSilva"> Hylan Silva</a><a 
+        style={{"textDecoration":"none","color":"white"}} href="#"> DSLabz</a> </TextBotton>
       </Botton>
-    </Div>
+    </Corpo>
   )
 }
 
